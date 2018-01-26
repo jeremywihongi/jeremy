1c99063fcd41b96c0f2baf31bebe50000763e848
jeremywihongi <35441450+jeremywihongi@users.noreply.github.com>
2018-01-26 15:52:24 +1300

    v1.2.9

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-
 // Carousel 
 $(window).load(function () {
 
-$('.main-carousel').flickity({
-  // options
-  cellAlign: 'left',
-  contain: true,
-  wrapAround: true,
-//    adaptiveHeight: true
-});
-    
-    $('.main-carousel2').flickity({
-  // options
-  cellAlign: 'left',
-  contain: true,
-  wrapAround: true,
-//        adaptiveHeight: true
-});
+    var hero_carousel = document.querySelector('.main-carousel');
+    var hero_flkty = new Flickity(hero_carousel, {
+        imagesLoaded: true,
+        autoPlay: 2500,
+        pauseAutoPlayOnHover: false,
+        wrapAround: true,
+        pageDots: true
+    });
+    var register_carousel = document.querySelector('.main-carousel1');
+    var register_flkty = new Flickity(register_carousel, {
+        imagesLoaded: true,
+        autoPlay: 6000,
+        pauseAutoPlayOnHover: false,
+        wrapAround: true,
+        pageDots: true
+    });
 
 });

f06d22e7d81358dcc4c7048b7e2218952915eedd
jeremywihongi <35441450+jeremywihongi@users.noreply.github.com>
2018-02-02 12:24:20 +1300

    v1.4.9

--- js/main.js
+++ js/main.js
@@ -2,56 +2,27 @@
 // Carousel 
 $(window).load(function () {
 
-  $('.main-carousel').flickity({
-			cellAlign: 'left',
-			cellSelector: '.carousel-cell',
-			contain: true,
-			prevNextButtons: true,
-			wrapAround: true,
-			pageDots: false,
-			draggable: false,
-			imagesLoaded: true,
-			accessibility: false,
-			selectedAttraction: 0.040, // Default 0.025
-			friction: 0.40 // Default 0.28
-		});
-
-		$('.project-images').each( function( i, container ) {
-		  var $container = $( container );
-
-		  var $carousel = $container.find('.main-carousel').flickity({
-				cellAlign: 'left',
-				cellSelector: '.carousel-cell',
-				contain: true,
-				prevNextButtons: true,
-				wrapAround: true,
-				pageDots: false,
-				draggable: false,
-				adaptiveHeight: true,
-				imagesLoaded: true
-			});
-
-		  var $counter = $container.find('.gallery-counter');
-		  // Flickity instance
-		  var flkty = $carousel.data('flickity');
-
-		  $carousel.on( 'select.flickity', function() {
-		    // set image caption using img's alt
-		    $counter.text( flkty.selectedElement.alt )
-		  });
-
-			function updateStatus() {
-				var cellNumber = flkty.selectedIndex + 1;
-				$counter.text( cellNumber + '/' + flkty.cells.length );
-			}
-
-			updateStatus();
-			$gallery.on( 'cellSelect.flickity', updateStatus );
-		});
-
-		function layout() {
-			startFlickity();
-		}
+$('.main-carousel').flickity({
+  // options
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true,
+  imagesLoaded: true,
+  autoPlay: true, 
+  bgLazyLoad: 1
+//    adaptiveHeight: true
+});
+    
+    $('.main-carousel2').flickity({
+  // options
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true,
+  imagesLoaded: true,
+  autoPlay: true,
+  bgLazyLoad: 1
+//        adaptiveHeight: true
+});
 
 });
 
